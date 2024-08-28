fc274af8c104846cedf31cfbb944d830a3a9dac9
riddhidudhat22 <ridhdhidudhat2003@gmail.com>
2024-08-28 13:24:09 +0530

    fdfs

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,7 +1,7 @@
 
 // export const baseURL = "http://localhost:8000/"
 
-export const baseURL = "http://localhost:8000/api/v1/"
+// export const baseURL = "http://localhost:8000/api/v1/"
 
 
-// export const baseURL="https://fruitable-server-three.vercel.app/api/v1/"
+export const baseURL="https://fruitable-server-three.vercel.app/api/v1/"
